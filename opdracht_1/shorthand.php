76414ed0d44d0e699c09a9c55832a65f9425496e
AngelMedinaSo <9030149@student.tcrmbo.nl>
2026-02-24 14:16:02 +0100

    Create shorthand.php

---
+++ opdracht_1/shorthand.php
@@ -0,0 +1,19 @@
+<?php
+$naam = "Angel";
+$leeftijd = 18;
+$favoriete_programmeertaal = "JavaScript"; 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Mijn naam is <?= $naam ?></p>
+    <p>Mijn leeftijd is <?= $leeftijd ?></p>
+    <p>Mijn favoriete programmeertaal is <?= $favoriete_programmeertaal ?></p>
+</body>
+</html>
