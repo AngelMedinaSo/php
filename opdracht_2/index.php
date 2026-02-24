bffa9fdb00666f8477537ffdad8c4abed2cf4e67
AngelMedinaSo <9030149@student.tcrmbo.nl>
2026-02-24 14:26:52 +0100

    Create index.php

---
+++ opdracht_2/index.php
@@ -0,0 +1,24 @@
+<?php
+$ingelogd = false;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>HOI!!!</h1>
+    <p>
+        <?php
+        if ($ingelogd) {
+            echo "Welkom terug!";
+        } else {
+            echo "Log eerst in.";
+        }
+        ?>
+    </p>
+</body>
+</html>
