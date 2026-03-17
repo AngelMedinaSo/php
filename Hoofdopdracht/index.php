b438acc9d0858a6a76e9e6f7490c80d66c9ed8d5
AngelMedinaSo <9030149@student.tcrmbo.nl>
2026-03-17 14:47:32 +0100

    hoofdopdracht

---
+++ Hoofdopdracht/index.php
@@ -0,0 +1,48 @@
+<?php
+
+require "includes/db.php";
+// Mijn mini-app wordt een: series die je kijkt tracker
+$appNaam = "Seriesly";
+$trackerType = "seriesTracker";
+$tagline = "Track ur favorite series and shows";
+
+$item = $conn->prepare("select * from tabel");
+$item->execute();
+$items = $item->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seriesly</title>
+</head>
+
+<body>
+    <h1>Hoi, welkom bij <?= $appNaam ?></h1>
+    <P><?= $tagline ?> using the best <?= $trackerType ?> available!</P>
+
+    <?php
+
+    if (count($items) === 0) {
+        echo "<p>Er zijn nog geen items toegevoegd.</p>";
+    } else {
+        echo "<ul>";
+
+        foreach ($items as $item) {
+            echo "<li>" . $item['titel'] . "</li>";
+        }
+
+        echo "</ul>";
+    }
+
+    ?>
+
+</body>
+
+<footer>&#169; <?= date('Y'), " ", $appNaam ?></footer>
+
+</html>
---
+++ Hoofdopdracht/pages/toevoegen.php
@@ -0,0 +1,10 @@
+<?php 
+    include "../includes/header.php";
+    include "../includes/nav.php";
+ ?>
+
+ <!-- Hier komt de inhoud van de pagina -->
+    <h1>The king Julian</h1>
+    <p>Hier komt later een formulier om een boek toe te voegen.</p>
+
+ <?php include "../includes/footer.php"; ?> 
