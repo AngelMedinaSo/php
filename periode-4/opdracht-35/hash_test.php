a71a59c8811c93b83d5a1e66e136b25ce74ea4c0
AngelMedinaSo <9030149@student.tcrmbo.nl>
2026-06-27 00:55:09 +0200

    Opdracht 35 - Simplify comments in English

--- periode-4/opdracht-35/hash_test.php
+++ periode-4/opdracht-35/hash_test.php
@@ -8,22 +8,6 @@ echo "Origineel wachtwoord: " . $wachtwoord;
 echo "<br>";
 echo "Hash: " . $hash;
 
-/*
-----------------------------------------------
-    DENKNAVRAGEN
-----------------------------------------------
-
-Waarom ziet de hash er elke keer anders uit?
-    password_hash() voegt automatisch een willekeurige
-    "salt" toe aan het wachtwoord voor het hashen.
-    Daardoor is de uitkomst elke keer anders,
-    ook al is het wachtwoord hetzelfde.
-
-Waarom is dat juist goed?
-    Als twee gebruikers hetzelfde wachtwoord hebben,
-    zien hun hashes er toch anders uit.
-    Hackers kunnen dan geen lijsten van bekende hashes
-    gebruiken om wachtwoorden te raden (rainbow tables).
-----------------------------------------------
-*/
+// The hash looks different every time because password_hash() adds a random salt.
+// This is good because two users with the same password will have different hashes.
 ?>
