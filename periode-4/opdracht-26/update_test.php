28db89d0b442e5f465674ca13229991b05c615d4
AngelMedinaSo <9030149@student.tcrmbo.nl>
2026-06-27 00:13:16 +0200

    Opdracht 26 - UPDATE query met prepared statement

---
+++ periode-4/opdracht-26/update_test.php
@@ -0,0 +1,25 @@
+<?php
+
+$servername = "localhost";
+$username   = "root";
+$password   = "";
+$dbname     = "items";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Verbinding mislukt: " . $e->getMessage();
+    exit;
+}
+
+// Testdata
+$titel = "Nieuwe titel";
+$id    = 1;
+
+$sql  = "UPDATE items SET titel = ? WHERE ID = ?";
+$stmt = $conn->prepare($sql);
+$stmt->execute([$titel, $id]);
+
+echo "Record bijgewerkt! ID " . $id . " heeft nu de titel: " . $titel;
+?>
