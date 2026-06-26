c1332111399286fa3ef48344aa58813e71457b31
AngelMedinaSo <9030149@student.tcrmbo.nl>
2026-06-27 00:45:23 +0200

    Update delete.php

--- periode-4/opdracht-29/delete.php
+++ periode-4/opdracht-29/delete.php
@@ -13,14 +13,12 @@ try {
     exit;
 }
 
-// Haal het ID op uit de URL
 $id = (int) $_GET['id'];
 
 $sql  = "DELETE FROM items WHERE ID = ?";
 $stmt = $conn->prepare($sql);
 $stmt->execute([$id]);
 
-// Ga terug naar de lijstpagina
 header("Location: index.php");
 exit;
 ?>
