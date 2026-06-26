0250cab3d06477139c67248bfcbce3d9f9df3b0b
AngelMedinaSo <9030149@student.tcrmbo.nl>
2026-06-27 01:03:32 +0200

    a

--- periode-4/opdracht-40/session_test.php
+++ periode-4/opdracht-40/session_test.php
@@ -5,7 +5,4 @@ session_start();
 $_SESSION['user'] = "Angelino";
 
 echo "Ingelogde gebruiker: " . $_SESSION['user'];
-
-// Session data is stored on the server. The browser only gets a session ID (cookie).
-// The data stays after refresh. It disappears when you close the browser or call session_destroy().
 ?>
--- periode-4/opdracht-41/check_login.php
+++ periode-4/opdracht-41/check_login.php
@@ -7,7 +7,4 @@ if (!isset($_SESSION['user'])) {
 } else {
     echo "Welkom, " . $_SESSION['user'] . "!";
 }
-
-// isset() checks if the variable exists before using it. Without it PHP gives an error.
-// If the session does not exist, isset() returns false and "Niet ingelogd" is shown.
 ?>
