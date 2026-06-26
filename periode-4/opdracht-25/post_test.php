af05b2cb92985b9c11c447b3070a594bc34bb937
AngelMedinaSo <9030149@student.tcrmbo.nl>
2026-06-27 00:35:11 +0200

    Update post_test.php

--- periode-4/opdracht-25/post_test.php
+++ periode-4/opdracht-25/post_test.php
@@ -1,5 +1,4 @@
 <?php
-// Verwerk het formulier als het verstuurd is
 $titel = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $titel = $_POST["titel"];
@@ -22,17 +21,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <button type="submit">Verstuur</button>
     </form>
 
-    <hr>
-
     <h2>Inhoud van $_POST:</h2>
     <?php
-        // Toon alle data in $_POST
         var_dump($_POST);
     ?>
 
     <h2>Waarde van titel:</h2>
     <?php
-        // Toon specifiek de waarde van titel
         if (!empty($titel)) {
             echo "Titel: " . $titel;
         } else {
